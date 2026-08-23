bb461ef01fbd794cf053ffcfad66b9635fd820b1
Dennis R. Friedrichsen <dennis.r.friedrichsen@gmail.com>
2026-08-23 10:19:35 -0500

    fix url

--- Formula/llama-model.rb
+++ Formula/llama-model.rb
@@ -1,7 +1,7 @@
 class LlamaModel < Formula
   desc "Manage and run local GGUF models with llama-server"
   homepage "https://github.com/dennisfriedrichsen/llama-model"
-  url "https://github.com/dennisfriedrichsen/llama-model/archive/refs/tags/v0.3.0.tar.gz"
+  url "https://github.com/dennisfriedrichsen/llama-model/archive/refs/tags/0.3.0.tar.gz"
   sha256 "d5558cd419c8d46bdc958064cb97f963d1ea793866414c025906ec15033512ed"
   license "BSD-2-Clause"
 
